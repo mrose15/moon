edab1fd35ba1c9924fdb0ee0b27f555128e735eb
Michele Cheow <ldnallen@gmail.com>
2023-08-10 08:21:08 -0700

    remove private api key from rapid api

--- src/components/Moon.jsx
+++ src/components/Moon.jsx
@@ -2,11 +2,6 @@ import axios from "axios";
 
 const options = {
   method: "GET",
-  url: "https://moon-phase.p.rapidapi.com/calendar",
-  headers: {
-    "X-RapidAPI-Key": "9872acfecbmsh7bd10ad8b0c4fccp15b00bjsnf71453c429d6",
-    "X-RapidAPI-Host": "moon-phase.p.rapidapi.com",
-  },
 };
 
 try {
